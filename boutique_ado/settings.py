054a7e627f0ecbad6a3f0e0496ac75b675e7e03c
manojkumar2kk11 <manojkumar2kk11@gmail.com>
2025-04-13 22:52:56 +0100

    Prepare for deploy

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-# import dj_database_url
+import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env  # needed to import local env vars, don't remove
@@ -31,6 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1', # local host
     'localhost', # listen for stripe webhooks
+    'boutique-ado-manoj-38368527bae5.herokuapp.com',
 ]
 
 
@@ -126,13 +127,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
